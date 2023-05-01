f8e31061bac7074f95abd2294bc18e99b3b1ed6e
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-05-01 12:11:45 -0400

    Updated resume

--- my-portfolio/src/app/resume/page.jsx
+++ my-portfolio/src/app/resume/page.jsx
@@ -4,8 +4,8 @@ export default function page() {
   return (
     <>
       <h1>Resume</h1>
-      <object data="/uploads/David Koll Resume.pdf" type="application/pdf" width="100%" height="500px" />
-      <Link href="/uploads/David Koll Resume.pdf">Download as PDF</Link>
+      <object data="/uploads/David_Koll_Resume.pdf" type="application/pdf" width="100%" height="500px" />
+      <Link href="/uploads/David_Koll_Resume.pdf">Download as PDF</Link>
     </>
   )
 
