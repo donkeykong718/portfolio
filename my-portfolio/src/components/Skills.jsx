d0074ac90f55ac47c952e63a88aa9672fa57b686
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-05-10 11:53:54 -0400

    Creates a main template-component for all the main sections

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -21,18 +21,10 @@ export default function Skills() {
   ];
 
   return (
-    <div id="skills" className="pt-20 w-full lg:h-full p-2">
-      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
-          Skills
-        </p>
-        <h2 className="py-4">What I Can Do</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {skillset.map((skill, index) => (
-            <Skill skill={skill} key={index} />
-          ))}
-        </div>
-      </div>
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+      {skillset.map((skill, index) => (
+        <Skill skill={skill} key={index} />
+      ))}
     </div>
   );
 }
